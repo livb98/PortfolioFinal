d968ee284569160b216252e79e8f81b688fc1aaa
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-04 11:50:00 +0300

    hg

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import dotenv from 'dotenv'
 import router from './route.js'
 dotenv.config();
 const app = express();
-// app.use(cors({origin: "http://localhost:5173", credentials: true}));
-app.use(cors({origin: ["http://localhost:5173",'https://portfoliofinal-7bli.onrender.com '], credentials: true}));
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
+// app.use(cors({origin: ["http://localhost:5173",'https://portfoliofinal-7bli.onrender.com '], credentials: true}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
