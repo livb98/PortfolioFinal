aa7bca63e864ba423ceca1d33c256b55aefb24e2
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-04 11:11:26 +0300

    c

--- portfolio/src/features/Contact/Contact.jsx
+++ portfolio/src/features/Contact/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${serverUrl}/contact`, formData);
+            const res = await axios.post(`http://127.0.0.1:3001/contact`, formData);
             const data = await res.data;
             console.log('Contact created successfully:', data);
         } catch (error) {
--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv'
 import router from './route.js'
 dotenv.config();
 const app = express();
-app.use(cors({origin: ["http://localhost:5173/",'https://portfoliofinal-7bli.onrender.com/contact '], credentials: true}));
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
+// app.use(cors({origin: ["http://localhost:5173/",'https://portfoliofinal-7bli.onrender.com/contact '], credentials: true}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
