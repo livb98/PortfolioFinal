e86f3bfa364f53c0c7dd50d91925998a92ea6f0e
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-06-10 09:38:29 +0300

    c

--- portfolio/src/features/Projects/Project.jsx
+++ portfolio/src/features/Projects/Project.jsx
@@ -1,12 +1,24 @@
-
+import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Project = () => {
     return (
         <div id="project" className="project">
-        <h1>PROJECTS</h1>
-    </div>
-    )
+            <FontAwesomeIcon icon={faFolderOpen} />
+            <h2>
+            <a 
+                className="projectLink" 
+                href="https://finalproject-1-ax9d.onrender.com" 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                MY PROJECTS
+            </a>
 
+            </h2>
+
+        </div>
+    );
 }
 
-export default Project
+export default Project;
